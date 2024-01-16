fa3e6a35d3c00e0f1127af9086520e7aa627d2c9
Vitor Martini <vitor-martini@outlook.com>
2024-01-15 23:09:30 -0300

    config: cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const rotaLivro = require("./routes/livro")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 const port = 8000
 
 app.use('/livros', rotaLivro)
